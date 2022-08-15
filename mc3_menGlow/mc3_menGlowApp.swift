bb6d4578ff818d349ab841443d59a417042cf995
“rickysilitonga112” <rickysilitonga112@gmail.com>
2022-08-15 17:55:00 +0700

    Initial Commit

---
+++ mc3_menGlow/mc3_menGlowApp.swift
@@ -0,0 +1,20 @@
+//
+//  mc3_menGlowApp.swift
+//  mc3_menGlow
+//
+//  Created by Ricky Silitonga on 15/08/22.
+//
+
+import SwiftUI
+
+@main
+struct mc3_menGlowApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
