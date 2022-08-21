e6a1092533adf5169688c262b6d7a2aa26f5f172
Amel9821 <amalia21198@gmail.com>
2022-08-21 23:51:28 +0700

    update progress view + add packages swift algorithm dan UICircularProgressRing

--- mc3_menGlow/views/screens/ProgressView.swift
+++ mc3_menGlow/views/screens/ProgressView.swift
@@ -6,20 +6,201 @@
 //
 
 import SwiftUI
+import UICircularProgressRing
 
 struct ProgressView: View {
+    
+    @StateObject var taskModel: TaskViewModel = TaskViewModel()
+    @Namespace var animation
+    @State var progress = RingProgress.percent(0.5)
+    
     var body: some View {
-        VStack{
-        Image("header_allpage")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-        Text("Progress")
+        VStack(spacing: 0){
+            Image("header_allpage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            VStack{
+            Text("Progress")
                 .font(.largeTitle)
-        }
-        
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal)
+            ScrollView(.horizontal, showsIndicators: false) {
+                
+                HStack(spacing: 10) {
+                    
+                    ForEach(taskModel.currentWeek, id: \.self){ day in
+                        VStack{
+                            Text(taskModel.extractDate(date: day, format: "EEE"))
+                                .font(.system(size: 17))
+                                .fontWeight(.bold)
+                            //EEE will return day as MON, TUE, ...etc
+                            Text(taskModel.extractDate(date: day, format: "dd"))
+                                .font(.system(size: 17))
+                        }
+                        //MARK: Foreground Style
+//                        .foregroundStyle(taskModel.isToday(date: day) ? .primary : .primary)
+                        .foregroundColor(taskModel.isToday(date: day) ? .white : .black)
+                        //MARK: Capsule Shape
+                        .frame(width: 42, height: 70)
+                        .background(
+                            ZStack {
+                                //MARK: Matched Geometry Effect
+                                if taskModel.isToday(date: day){
+                                    Capsule()
+                                        .fill(Color("RingColor"))
+                                        .matchedGeometryEffect(id: "CURRENTDAY", in: animation)
+                                }
+                            }
+                        )
+                        .contentShape(Capsule())
+                        .onTapGesture {
+                            //updating current day
+                            withAnimation {
+                                taskModel.currentDate = day
+                            }
+                        }
+                    }
+                }
+                .padding(.horizontal)
+            }
+//            Spacer()
+//                .frame(height: 26)
+            GoalProgressRing(
+                ringColor: Color("RingColor"),
+                progressValue: .constant(0.5)
+            )
+            .frame(maxHeight: 188, alignment: .top)
+            HStack{
+                Circle()
+                    .fill(Color("OuterColor"))
+                    .frame(width: 27, height: 27)
+                Text("To do")
+                    .font(.system(size: 17))
+                    .fontWeight(.semibold)
+                Spacer()
+                    .frame(width: 70)
+                Circle()
+                    .fill(Color("RingColor"))
+                    .frame(width: 27, height: 27)
+                Text("Done")
+                    .font(.system(size: 17))
+                    .fontWeight(.semibold)
+                
+            }
+            .padding()
+            
+            ZStack{
+                RoundedRectangle(cornerRadius: 10, style: .circular)
+                
+                    .fill(.white)
+                
+                    .frame(width: 350, height: 40)
+                    .shadow(radius: 1)
+                HStack{
+                    Text("Goals")
+                        .font(.system(size: 17))
+                        .fontWeight(.semibold)
+                    Spacer()
+                        .frame(width: 180)
+                    Text("3/7 days")
+                        .font(.system(size: 17))
+                        .fontWeight(.semibold)
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .frame(width: 10, height: 18)
+                        .foregroundColor(Color("Brown"))
+                }
+            }
+//            Spacer()
+//                .frame(height: 26)
+            Text("Streaks")
+                .font(.system(size: 17))
+                .fontWeight(.semibold)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.horizontal, 38)
+            HStack{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10, style: .circular)
+                    
+                        .fill(.white)
+                    
+                        .frame(width: 165, height: 120)
+                        .shadow(radius: 1)
+                    VStack{
+                        Text("Current Streak")
+                            .font(.system(size: 17))
+                            .fontWeight(.semibold)
+                        Text("3")
+                            .font(.system(size: 48))
+                            .fontWeight(.bold)
+                        Text("days")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                    }
+                }
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10, style: .circular)
+                    
+                        .fill(.white)
+                    
+                        .frame(width: 165, height: 120)
+                        .shadow(radius: 1)
+                    VStack{
+                        Text("Best Streak")
+                            .font(.system(size: 17))
+                            .fontWeight(.semibold)
+                        Text("3")
+                            .font(.system(size: 48))
+                            .fontWeight(.bold)
+                        Text("days")
+                            .font(.system(size: 24))
+                            .fontWeight(.bold)
+                    }
+                }
+                
+            }
+            
+            
+        }}
+        .ignoresSafeArea()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("BackgroundColor"))
     }
 }
 
+private struct GoalProgressRing: View {
+    let ringColor: Color
+    let outerColor = Color("OuterColor")
+    @Binding var progressValue: Double
+    
+    var body: some View{
+        VStack {
+            ProgressRing(
+                progress: Binding(get: {
+                    RingProgress.percent(progressValue) }, set: {
+                        progressValue = $0.asDouble! }),
+                outerRingStyle: .init(
+                    color: .color(outerColor),
+                    strokeStyle: .init(lineWidth: 30)
+                ),
+                innerRingStyle: .init(
+                    color: .color(ringColor),
+                    strokeStyle: .init(lineWidth: 30,
+                                       lineCap: .round),
+                    padding: 3
+                ),
+                { value in
+                    Label(
+                        String(Int(value * 100)), image:""
+                    )
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                }
+            )
+        }
+    }
+}
 struct ProgressView_Previews: PreviewProvider {
     static var previews: some View {
         ProgressView()
